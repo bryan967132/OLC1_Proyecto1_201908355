7e25222c53e59554218945a48f4818a1f4636f96
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-02-22 03:02:03 -0600

    Test de Interfaz Completo

---
+++ src/Tests/InterfaceTest.java
@@ -0,0 +1,8 @@
+package Tests;
+import Interface.Window;
+public class InterfaceTest {
+    public static void main(String[] args) {
+        Window w = new Window();
+        w.setVisible(true);
+    }
+}
