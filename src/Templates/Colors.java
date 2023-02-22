9e3723bc311000ca98d215769552fe225bbd4783
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-02-22 02:17:47 -0600

    Plantillas Componentes GUI

--- src/Templates/Colors.java
+++ src/Templates/Colors.java
@@ -2,8 +2,8 @@ package Templates;
 import java.awt.Color;
 public class Colors{
 	public static Color LIGHTECLIPSE = new Color(128,128,128);//rgb(128,128,128)
-	public static Color MEDIUMCLIPSE1 = new Color(81,86,88);//rgb(81,86,88)
-	public static Color MEDIUMCLIPSE2 = new Color(64,64,64);//rgb(64,64,64)
+	public static Color MEDIUMECLIPSE1 = new Color(81,86,88);//rgb(81,86,88)
+	public static Color MEDIUMECLIPSE2 = new Color(64,64,64);//rgb(64,64,64)
 	public static Color DARKECLIPSE = new Color(47,47,47);//rgb(47,47,47)
 	public static Color GREEN1 = new Color(0,163,146);//rgb(43,122,11)
 	public static Color GREEN2 = new Color(0,192,172);//rgb(91,179,24)
