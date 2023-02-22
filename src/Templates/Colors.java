a885f343e7187a042dd02b6d66f0396fb839f8fa
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-02-22 06:09:39 -0600

    Plantillas Componentes GUI

--- src/Templates/Colors.java
+++ src/Templates/Colors.java
@@ -5,9 +5,9 @@ public class Colors{
 	public static Color MEDIUMECLIPSE1 = new Color(81,86,88);//rgb(81,86,88)
 	public static Color MEDIUMECLIPSE2 = new Color(64,64,64);//rgb(64,64,64)
 	public static Color DARKECLIPSE = new Color(47,47,47);//rgb(47,47,47)
-	public static Color GREEN1 = new Color(0,163,146);//rgb(43,122,11)
-	public static Color GREEN2 = new Color(0,192,172);//rgb(91,179,24)
-	public static Color GREEN3 = new Color(142,207,106);//rgb(125,206,19)
+	public static Color GREEN1 = new Color(43,122,11);//rgb(43,122,11)
+	public static Color GREEN2 = new Color(91,179,24);//rgb(91,179,24)
+	public static Color GREEN3 = new Color(125,206,19);//rgb(125,206,19)
 	public static Color RED = new Color(232,17,35);//rgb(232,17,35)	
 	public static Color WHITE = new Color(255,255,255);//rgb(255,255,255)
 }
