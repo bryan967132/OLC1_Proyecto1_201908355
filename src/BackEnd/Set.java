a9488d9563ba62e4bec79cb444f14ab5795b7acd
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-02-27 16:58:31 -0600

    Estructura Set (Conjunto)

--- src/BackEnd/Set.java
+++ src/BackEnd/Set.java
@@ -1,10 +1,10 @@
 package BackEnd;
 import java.util.ArrayList;
 public class Set {
-    public String id = null;
-    public String startChar = null;
-    public String endChar = null;
-    public ArrayList<String> specifics = null;
+    public String id;
+    public String startChar;
+    public String endChar;
+    public ArrayList<String> specifics;
     public String toString() {
         return "ID: " + id + (specifics != null ? "\t\tSpecifics: " + specifics : "\t\tStartChar: " + startChar + "\t\t\tEndChar: " + endChar);
     }
