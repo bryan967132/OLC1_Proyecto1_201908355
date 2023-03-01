82abfff9b938bddf37984117be4bac82b7be0f12
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-02-28 21:08:17 -0600

    Controladores de Análisis

---
+++ src/BackEnd/Expression.java
@@ -0,0 +1,8 @@
+package BackEnd;
+public class Expression {
+    public String id;
+    public String string;
+    public String toString() {
+        return "EXPRESIÓN -------------- ID: " + id + " ".repeat(20 - id.length()) + "STRING: " + string;
+    }
+}
---
+++ src/BackEnd/Set.java
@@ -0,0 +1,11 @@
+package BackEnd;
+import java.util.ArrayList;
+public class Set {
+    public String id;
+    public String startChar;
+    public String endChar;
+    public ArrayList<String> specifics = new ArrayList<>();
+    public String toString() {
+        return "CONJUNTO --------------- ID: " + id + (specifics.size() > 0 ? " ".repeat(20 - id.length()) + "SPECIFICS: " + String.join(", ",specifics) : " ".repeat(20 - id.length()) + "STARTCHAR: " + startChar + "\t\t\tENDCHAR: " + endChar);
+    }
+}
