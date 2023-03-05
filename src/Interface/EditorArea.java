75ba376904bb4b2d9169b0f71752b7811d3d4cbd
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-03-05 17:19:09 -0600

    Estilo de Edicion JTextPane

--- src/Interface/EditorArea.java
+++ src/Interface/EditorArea.java
@@ -6,8 +6,19 @@ import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.BoxView;
+import javax.swing.text.ComponentView;
+import javax.swing.text.DefaultEditorKit;
 import javax.swing.text.Document;
-
+import javax.swing.text.Element;
+import javax.swing.text.IconView;
+import javax.swing.text.LabelView;
+import javax.swing.text.ParagraphView;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledEditorKit;
+import javax.swing.text.View;
+import javax.swing.text.ViewFactory;
 import Colors.Colors;
 public class EditorArea extends JPanel {
     JTextPane editor;
@@ -20,12 +31,19 @@ public class EditorArea extends JPanel {
         setPreferredSize(new Dimension (30, 30));
         setMinimumSize(new Dimension (30, 30));
         //editor
-        editor = new JTextPane(){
+        editor = new JTextPane() {
             public void paint(Graphics g) {
                 super.paint(g);
                 EditorArea.this.repaint();
             }
         };
+        editor.setEditorKit(
+            new StyledEditorKit() {
+                public ViewFactory getViewFactory() {
+                    return new NoWrapViewFactory();
+                }
+            }
+        );
         editor.setForeground(Colors.WHITE);
         editor.setBackground(Colors.DARKECLIPSE);
         editor.setFont(new java.awt.Font("Consolas", 0, 11));
@@ -52,5 +70,42 @@ public class EditorArea extends JPanel {
         for(int line = startline, y = 0; line <= endline; line ++, y += fontSize) {
             g.drawString(Integer.toString(line),0,y);
         }
+    }
+}
+class NoWrapEditorKit extends DefaultEditorKit {
+    private static final long serialVersionUID = 1L;
+    public ViewFactory getViewFactory() {
+        return new NoWrapViewFactory();
+    }
+}
+class NoWrapViewFactory implements ViewFactory {
+    public View create(Element element) {
+        String kind = element.getName();
+        if (kind != null) {
+            if (kind.equals(AbstractDocument.ContentElementName)) {
+                return new LabelView(element);
+            } else if (kind.equals(AbstractDocument.ParagraphElementName)) {
+                return new NoWrapParagraphView(element);
+            } else if (kind.equals(AbstractDocument.SectionElementName)) {
+                return new BoxView(element, View.Y_AXIS);
+            } else if (kind.equals(StyleConstants.ComponentElementName)) {
+                return new ComponentView(element);
+            } else if (kind.equals(StyleConstants.IconElementName)) {
+                return new IconView(element);
+            }
+        }
+        return new LabelView(element);
+    }
+}
+
+class NoWrapParagraphView extends ParagraphView {
+    public NoWrapParagraphView(Element elem) {
+        super(elem);
+    }
+    public void layout(int width, int height) {
+        super.layout(Short.MAX_VALUE, height);
+    }
+    public float getMinimumSpan(int axis) {
+        return super.getPreferredSpan(axis);
     }
 }
